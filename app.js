977aeabb7396cdf98dd860e08eaebcceefde850b
Isbiengood <lawlessmaison@gmail.com>
2026-06-11 22:01:28 +0200

    Add alert on app start

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+alert("APP DEMARREE");
 const messageInput = document.getElementById("message");
 const btnCodeRouge = document.getElementById("btn-code-rouge");
 const statusDiv = document.getElementById("status");
